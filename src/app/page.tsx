35d8f8d4616577a824549767194889c3f4d3760e
Jawdat <jawdat.89@gmail.com>
2023-11-04 21:24:00 +0200

    Add home header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import HomeHeader from "@/app/components/HomeHeader";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold text-center">Home Page</h1>
-    </main>
+    <>
+      <HomeHeader />
+    </>
   );
 }
