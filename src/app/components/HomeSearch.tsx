d51c94cfdefa43e9d333aa8b3647372ddb41a991
Jawdat <jawdat.89@gmail.com>
2023-11-04 22:31:55 +0200

    Add body section

---
+++ src/app/components/HomeSearch.tsx
@@ -0,0 +1,77 @@
+"use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import React, { FormEvent, useState } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { BsFillMicFill } from "react-icons/bs";
+
+export default function HomeSearch() {
+  const router = useRouter();
+  const [input, setInput] = useState<string>("");
+  const [randomSearchLoading, setRandomSearchLoading] =
+    useState<boolean>(false);
+
+  const submit = (searchTerms: string = input) => {
+    if (!input.trim()) return;
+
+    router.push(`/search/web?searchTerm=${searchTerms}`);
+  };
+
+  function handleSubmitForm(e: FormEvent<HTMLFormElement>): void {
+    e.preventDefault();
+    submit();
+  }
+
+  async function randomSearch() {
+    setRandomSearchLoading(true);
+    const res = await fetch("https://random-word-api.herokuapp.com/word");
+    const data = await res.json();
+
+    if (!data || !data.length || data[0].trim() === "") return;
+
+    submit(data[0]);
+    setRandomSearchLoading(false);
+  }
+
+  return (
+    <>
+      <form
+        className="flex w-full mt-5 mx-auto max-w-[90%] sm:max-w-xl lg:max-w-2xl  border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow"
+        onSubmit={handleSubmitForm}
+      >
+        <AiOutlineSearch className="texl-xl text-gray-500 mr-3" />
+
+        <input
+          type="text"
+          className="flex-grow focus:outline-none"
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+        />
+
+        <BsFillMicFill className="text-lg" />
+      </form>
+      <div className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4 justify-center mt-8">
+        <button className="btn" onClick={() => submit()}>
+          Google Search
+        </button>
+        <button
+          className="btn flex items-center justify-center disabled:opacity-80"
+          disabled={randomSearchLoading}
+          onClick={randomSearch}
+        >
+          {randomSearchLoading ? (
+            <Image
+              className="animate-spin h-6 text-center"
+              src="spinner.svg"
+              width={20}
+              height={20}
+              alt="Loading..."
+            />
+          ) : (
+            "I Am Feeling Lucky"
+          )}
+        </button>
+      </div>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 import HomeHeader from "@/app/components/HomeHeader";
+import Image from "next/image";
+import HomeSearch from "@/app/components/HomeSearch";
 
 export default function Home() {
   return (
     <>
+      {/* Header */}
       <HomeHeader />
+
+      {/* Body */}
+      <div className="flex flex-col items-center mt-24">
+        <Image
+          width={300}
+          height={100}
+          src={
+            "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+          }
+          alt={"Google"}
+        />
+
+        <HomeSearch />
+      </div>
     </>
   );
 }
